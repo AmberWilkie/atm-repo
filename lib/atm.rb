a6e61f72ab5af1393fcacb4fe81efcd3b0aaab2a
felixottander <felix.ottander@yahoo.com>
2017-02-07 14:55:49 +0100

    Added a card_disbaled method and set a test and when statement for disabled account_status.

--- lib/atm.rb
+++ lib/atm.rb
@@ -17,6 +17,8 @@ class Atm
       { status: false, message: 'wrong pin', date: Date.today }
     when card_expired?(account.exp_date)
       { status: false, message: 'card expired', date: Date.today }
+    when card_disabled?(account.account_status)
+      { status: false, message: 'card disabled', date: Date.today }
     else
       perform_transaction(amount, account)
     end
@@ -44,5 +46,9 @@ class Atm
 
   def card_expired?(exp_date)
     Date.strptime(exp_date, '%m/%y') < Date.today
+  end
+
+  def card_disabled?(account_status)
+    account_status == :disabled
   end
 end

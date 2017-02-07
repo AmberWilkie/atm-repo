61dc169aa85614364b9f537e8a65f498b47739ff
felixottander <felix.ottander@yahoo.com>
2017-02-07 15:43:46 +0100

    changed the perform_transaction method to include bills. Defined a add_bills method and set it to include 20,10&5 bills and while loop these bills until amount - bill > 0.

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -23,7 +23,7 @@ describe Atm do
   end
 
   it 'allow withdraw if account has enough balance.' do
-    expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+    expected_output = { status: true, message: 'success', date: Date.today, amount: 45, bills: [20, 20, 5] }
     expect(subject.withdraw(45, '1234', account)).to eq expected_output
   end
 
@@ -49,4 +49,5 @@ describe Atm do
     expected_output = {status: false, message: 'card disabled', date: Date.today }
     expect(subject.withdraw(40, '1234', account)).to eq expected_output
   end
+
 end

afb4ff14d69aa5a3d40587996b0700a3beba360b
felixottander <felix.ottander@yahoo.com>
2017-02-08 14:35:39 +0100

    added check length of pin and 2 tests on initialize. Which has the methods generate pin and set exp date iside the Account class.

---
+++ lib/account.rb
@@ -0,0 +1,24 @@
+class Account
+
+attr_accessor :exp_date, :pin_code, :balance, :account_status, :owner
+
+STANDARD_VALIDITY_YRS = 5
+
+ def initialize
+   @exp_date = set_exp_date
+   @pin_code = generate_pin
+   @balance = 0
+   @account_status = :active
+ end
+
+private
+
+def set_exp_date
+   Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime("%m/%y")
+end
+
+def generate_pin
+  rand(1000..9999)
+end
+
+end
---
+++ spec/account_spec.rb
@@ -0,0 +1,20 @@
+require './lib/account.rb'
+
+  describe Account do
+      let(:person) { instance_double('Person', name: 'Felix')}
+
+   it 'check length of a pin_code' do
+     number = 1234
+     number_length = Math.log10(number).to_i + 1
+     expect(number_length).to eq 4
+   end
+
+   it 'is expected to have balance of 0 on initialize' do
+     expect(subject.balance).to eq 0
+   end
+
+   it 'is expected to have an expiry date on initialize' do
+     expected_date = Date.today.next_year(5).strftime("%m/%y")
+     expect(subject.exp_date).to eq expected_date
+   end
+end

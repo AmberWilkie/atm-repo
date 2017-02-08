f322152dbe7bae398c4c5225cbee9ab1a0576edb
felixottander <felix.ottander@yahoo.com>
2017-02-08 16:57:42 +0100

    created Person class and added attr_accessors, set_name and missing_name methods. Also set @cash = 0.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+
+  attr_accessor :name, :cash, :account
+
+def initialize(attrs = {})
+  set_name(attrs[:name])
+  @cash = 0
+end
+
+private
+
+def set_name(obj)
+   obj == nil ? missing_name : @name = obj
+end
+
+def missing_name
+  raise 'An Account name is required'
+end
+
+end

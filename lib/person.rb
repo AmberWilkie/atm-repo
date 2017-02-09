f4605c3023c508d5ef26e7ce4107e0abf3bf7d5d
felixottander <felix.ottander@yahoo.com>
2017-02-09 16:47:34 +0100

    created withdraw method and increase_cash method to include the amount from the atm.withdraw method.

--- lib/person.rb
+++ lib/person.rb
@@ -19,12 +19,28 @@ def deposit(amount)
   @account == nil ? missing_account : deposit_funds(amount)
 end
 
+def withdraw(args = {})
+  @account == nil ? missing_account : withdraw_funds(args)
+end
 
 private
 
 def deposit_funds(amount)
   @cash -= amount
   @account.balance += amount
+end
+
+def withdraw_funds(args)
+  args[:atm] == nil ? missing_atm : atm = args[:atm]
+  account = @account
+  amount = args[:amount]
+  pin = args[:pin]
+  response = atm.withdraw(amount, pin, account)
+  response[:status] == true ? increase_cash(amount) : response
+end
+
+def increase_cash(amount)
+  @cash += amount
 end
 
 def set_name(obj)
@@ -39,5 +55,8 @@ def missing_account
    raise RuntimeError, 'No account present'
 end
 
+def missing_atm
+  raise RuntimeError, 'An ATM is required'
+end
 
 end
